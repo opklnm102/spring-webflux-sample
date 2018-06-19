3126db5e61e3103b9f02326a030fcceffcc6f664
Dong Hee Kim <opklnm102@gmail.com>
2018-06-19 14:00:58 +0900

    initial commit

---
+++ webflux-workshop/src/main/java/me/dong/webfluxworkshop/WebfluxWorkshopApplication.java
@@ -0,0 +1,12 @@
+package me.dong.webfluxworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxWorkshopApplication.class, args);
+	}
+}
---
+++ webflux-workshop/src/test/java/me/dong/webfluxworkshop/WebfluxWorkshopApplicationTests.java
@@ -0,0 +1,16 @@
+package me.dong.webfluxworkshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebfluxWorkshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
